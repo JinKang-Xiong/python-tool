df4b27b7e89b9913cdf49bba42830b59a2d9c7e7
xjk <2385472291@qq.com>
2024-12-19 10:40:08 +0800

    fix:增加测试文件

--- Test/test1.py
+++ Test/test1.py
@@ -1,9 +1,10 @@
+# 测试文件
 import os
 
 print(os.getcwd())  # 输出当前工作目录
 
-output_dir = os.path.join(os.getcwd(), 'src/store/out/')
+output_dir = os.path.join(os.getcwd(), "src/store/out/")
 
 print(output_dir)
 
-print(os.path.exists('src/store/out/'))
+print(os.path.exists("src/store/out/"))
